16ac61c8e835b98d9d90642097912597de68f953
Chibikai <110368326+Chibikai@users.noreply.github.com>
2023-04-07 18:37:33 -0500

    Updating Files, Getting data for research, Work on Data Comparison

---
+++ Week 3/Goldman Sachs - 24 Year Stock Price History _ GS _ MacroTrends_files/in.php
@@ -0,0 +1,10 @@
+// static32 (☞ﾟ∀ﾟ)☞
+if( window._cgen ) {
+_cgen.set_cookie('_jsuid', '3746025752');
+_cgen.advanced();
+_cgen.ping_start();
+_cgen.set_cookie('_heatmaps_g2g_100827248', 'no', 600 );
+_heatmaps_g2g_100827248='no';
+_cgen.set_cookie('_first_pageview', 1, 600 );
+_cgen.set_cookie('_referrer_og', 'https://www.google.com/', 30*86400 );
+}
