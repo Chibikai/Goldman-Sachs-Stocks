5ebfa6c40de165a2643244114bd5ef12411a1ccb
Kori Gray <Ko.gray@icloud.com>
2023-04-09 22:23:44 -0400

    LSTM  prediction model upload for GS, MS,Schwab

---
+++ Week 2/Time stamp data wrangling in R/Untitled.R
@@ -0,0 +1,12 @@
+install.packages("tidyverse")
+install.packages("tidyquant")
+
+library(readr)
+library(dplyr)
+library(tidyr)
+library(tidyquant) 
+library(timetk)
+
+GS<-read.csv("/Users/korigray/Documents/GitHub/Final class project/Final-Project/GS.csv")
+
+head(GS)
